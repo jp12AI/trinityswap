d8c68dfe265f90aae6ee3435927dd01961626c13
jp12AI <58639429+jp12AI@users.noreply.github.com>
2022-04-15 12:30:00 -0700

    Initializing the files
    File release

---
+++ src/App.tsx
@@ -0,0 +1,31 @@
+import React from "react";
+import "./App.less";
+import GitHubButton from "react-github-btn";
+import { Routes } from "./routes";
+
+function App() {
+  return (
+    <div className="App">
+      <div className="Banner">
+        <div className="Banner-description">
+          Trinityswap is currently beta software. Use at your own risk.
+        </div>
+      </div>
+      <Routes />
+      <div className="social-buttons">
+        <GitHubButton
+          href="https://github.com/jp12AI/solana-dex"
+          data-color-scheme="no-preference: light; light: light; dark: light;"
+          data-icon="octicon-star"
+          data-size="large"
+          data-show-count={true}
+          aria-label="Star solana-dex on GitHub"
+        >
+          Fork
+        </GitHubButton>
+      </div>
+    </div>
+  );
+}
+
+export default App;
